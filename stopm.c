39d7293a38db17fcb3d0bbcf5a4b9317b56e86a2
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-08-18 13:53:59 +1000

    Update stopm.c

--- stopm.c
+++ stopm.c
@@ -5,7 +5,7 @@
 int main () {
    char command[150];
 
-   strcpy( command, "tmux send -t world.0 'stop' ENTER && tmux send -t other.0 'C-c' ENTER" );
+   strcpy( command, "tmux send -t world.0 'stop && exit' ENTER && tmux send -t other.0 'C-c' ENTER && tmux send -t other.0 'exit' ENTER" );
    system(command);
 
    return(0);
