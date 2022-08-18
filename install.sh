67bd4d7be89ec2feb36ef41cad393b9c75e4ee26
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-08-18 13:38:22 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-sudo gcc main.c -o startm && sudo mv startm /bin && gcc attacho.c -o atcho && sudo mv atcho /bin && gcc attachw.c -o atchw && sudo mv atchw /bin
+sudo gcc main.c -o startm && sudo mv startm /bin && gcc attacho.c -o atcho && sudo mv atcho /bin && gcc attachw.c -o atchw && sudo mv atchw /bin && gcc stopm.c -o stopm && sudo mv stopm /bin
