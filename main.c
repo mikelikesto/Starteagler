a2fc1261161cb56ceb40dd9807f80581e61d2e24
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-11-08 12:38:09 +1100

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main () {
    char command[150];
 
-   strcpy( command, "tmux new  -d -s world && tmux new -d -s other && tmux send -t world.0 'cd /home/$USER/java/bukkit_command && ./run_unix.sh' ENTER && tmux send -t other.0 'cd /home/$USER/java/bungee_command && ./run_unix.sh' ENTER" );
+   strcpy( command, "tmux new  -d -s world && tmux new -d -s other && tmux send -t world.0 'cd /home/$USER/1.18.2 && java -jar spigot-1.18.2.jar' ENTER && tmux send -t other.0 'cd /home/$USER/1.18.2/ayungee/target/ && java -jar ayungee-1.0-SNAPSHOT-jar-with-dependencies.jar' ENTER" );
    system(command);
 
    return(0);
